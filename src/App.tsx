0b6ea7f49932e34d045dac15a11a65d6d68c3728
Fernandes Alves Fabien <fabien@code42.fr>
2022-08-01 07:20:35 +0200

    [#?] Added : Routing + some login screen improvement

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import LoginScreen from "./components/LoginScreen";
 import "./assets/scss/index.scss";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <LoginScreen />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginScreen />} />
+          <Route path="/dashboard" element={<></>} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
