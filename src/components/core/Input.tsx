5c87f96ef3fffb5b716aeea62e8895f32abc7354
Fernandes Alves Fabien <fabien@code42.fr>
2022-08-04 06:40:59 +0200

    [#?] Added : Input component

--- src/components/core/Input.tsx
+++ src/components/core/Input.tsx
@@ -6,6 +6,7 @@ interface InputProps {
   placeholder?: string;
   value?: string;
   focus?: boolean;
+  disabled?: boolean;
 }
 
 const Input: React.FC<InputProps> = ({
@@ -14,10 +15,11 @@ const Input: React.FC<InputProps> = ({
   value = "",
   placeholder = "",
   focus = false,
+  disabled = false,
 }: InputProps) => {
   return (
     <div className="input">
-      <label className="input__label">{label}</label>
+      <label className="input__label small-text-regular">{label}</label>
       <input
         type="text"
         value={value}
@@ -25,6 +27,7 @@ const Input: React.FC<InputProps> = ({
         autoFocus={focus}
         onChange={onChange}
         className="input__field"
+        disabled={disabled}
       />
     </div>
   );
