55d7a94666afc4314eef80a8a8decec3496879e6
Fernandes Alves Fabien <fabien@code42.fr>
2022-08-04 06:52:21 +0200

    [#?] Changed : Rename login to splashscreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import { useState } from "react";
-import LoginScreen from "./components/LoginScreen";
 import "./assets/scss/index.scss";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Input from "./components/core/Input";
+import Container from "./components/core/Container";
+import SplashScreen from "./components/SplashScreen";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LoginScreen />} />
+          <Route path="/" element={<SplashScreen />} />
           <Route
             path="/dashboard"
             element={
-              <>
+              <Container>
                 <Input
                   label="Label"
                   placeholder="Placeholder"
                   onChange={() => {}}
                   disabled
                 />
-              </>
+              </Container>
             }
           />
           <Route path="*" element={<Navigate to="/" replace />} />
